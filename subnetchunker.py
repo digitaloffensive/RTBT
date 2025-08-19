5721df2f42cad5318a877baa8a62b1fdf7f073f7
Michael LaSalvia <genxweb@gmail.com>
2025-08-19 15:21:57 -0400

    Update subnetchunker.py
    spelling

--- subnetchunker.py
+++ subnetchunker.py
@@ -1,6 +1,6 @@
 ## Break down Subnet.
 ## Since some tools cant not handle scanning multiple large networks this tool will break down your subnets into chunks that the tool can handle.
-## IE. Tenable Nessus chokes on it self after 2500 so scannign a /16 can be problematic. So you cna use this to break the /16 down in 24 chnunks using this.
+## IE. Tenable Nessus chokes on it self after 2500 so scaning a /16 can be problematic. So you cna use this to break the /16 down in 24 chnunks using this.
 ## It will create a csv with each chunk, usally a /16 will generate 27 files, last one is the remaidner if the math is not even ever.
 ##
 ## Created by Michael AKA Genxweb
